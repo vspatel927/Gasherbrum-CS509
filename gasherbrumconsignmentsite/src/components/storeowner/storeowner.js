6c8c917b74dc883ea411517602744b6261d969cd
Vansh Patel <vspatel@wpi.edu>
2023-11-06 22:40:21 -0500

    Code formatting

--- gasherbrumconsignmentsite/src/components/storeowner/storeowner.js
+++ gasherbrumconsignmentsite/src/components/storeowner/storeowner.js
@@ -12,22 +12,22 @@ function StoreOwner() {
         <div class="container">
             <header class="header">
                 <h1>{name} Computer Store </h1>
-                <input type="text" placeholder="Search..."  style={{ width: '50%' }} />
+                <input type="text" placeholder="Search..." style={{ width: '50%' }} />
             </header>
-            <div style={{float: 'right'}}>
+            <div style={{ float: 'right' }}>
                 <h4>Balance: $0</h4>
             </div>
             <table style={{ width: '75%', margin: '0 auto', marginTop: '0', borderCollapse: 'collapse', border: '1px sold #ccc' }}>
-        <tbody><tr>
-            <td class="site-td"> 
-            <AddComputerForm name={name} />
-            
-            <button name="generateStoreInventory" onClick={handleClick}>Generate Inventory</button>
-            {showComponent && <GenerateInventory name={name} />}
-            </td></tr></tbody></table>
+                <tbody><tr>
+                    <td class="site-td">
+                        <AddComputerForm name={name} />
+
+                        <button name="generateStoreInventory" onClick={handleClick}>Generate Inventory</button>
+                        {showComponent && <GenerateInventory name={name} />}
+                    </td></tr></tbody></table>
             <footer class="footer">
-    � 2023 Gasherbrum Project Groups
-</footer>
+                � 2023 Gasherbrum Project Groups
+            </footer>
         </div>
     );
 }
@@ -45,66 +45,66 @@ function AddComputerForm(props) {
         <div className='addComputerForm'>
             <h1>Add Computer</h1>
             <table>
-        <tbody><tr>
-            <td class="site-td">
-            <label for="price">Enter Price:</label>
-            <input
-                type="number"
-                placeholder="Price"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-            /> <br></br>
+                <tbody><tr>
+                    <td class="site-td">
+                        <label for="price">Enter Price:</label>
+                        <input
+                            type="number"
+                            placeholder="Price"
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        /> <br></br>
 
-            <label for="memory">Memory:</label>
-            <select value={memory} onChange={(e) => setMemory(e.target.value)}>
-                <option value="32 GB">32 GB</option>
-                <option value="16 GB">16 GB</option>
-                <option value="12 GB">12 GB</option>
-                <option value="8 GB">8 GB</option>
-                <option value="4 GB">4 GB</option>
-                <option value="1 GB">1 GB</option>
-            </select><br></br>
+                        <label for="memory">Memory:</label>
+                        <select value={memory} onChange={(e) => setMemory(e.target.value)}>
+                            <option value="32 GB">32 GB</option>
+                            <option value="16 GB">16 GB</option>
+                            <option value="12 GB">12 GB</option>
+                            <option value="8 GB">8 GB</option>
+                            <option value="4 GB">4 GB</option>
+                            <option value="1 GB">1 GB</option>
+                        </select><br></br>
 
-            <label for="storage">Storage Size:</label>
-            <select value={storage} onChange={(e) => setStorage(e.target.value)}>
-                <option value="2 TB">2 TB</option>
-                <option value="1 TB">1 TB</option>
-                <option value="512 GB">512 GB</option>
-                <option value="256 GB">256 GB</option>
-                <option value="128 GB">128 GB</option>
-            </select><br></br>
+                        <label for="storage">Storage Size:</label>
+                        <select value={storage} onChange={(e) => setStorage(e.target.value)}>
+                            <option value="2 TB">2 TB</option>
+                            <option value="1 TB">1 TB</option>
+                            <option value="512 GB">512 GB</option>
+                            <option value="256 GB">256 GB</option>
+                            <option value="128 GB">128 GB</option>
+                        </select><br></br>
 
-            <label for="processor">Processor:</label>
-            <select value={processor} onChange={(e) => setProcessor(e.target.value)}>
-                <option value="Intel Xeon">Intel Xeon</option>
-                <option value="Intel i9">Intel i9</option>
-                <option value="Intel i7">Intel i7</option>
-                <option value="AMD Ryzen 9">AMD Ryzen 9</option>
-                <option value="AMD Ryzen 7">AMD Ryzen 7</option>
-            </select><br></br>
+                        <label for="processor">Processor:</label>
+                        <select value={processor} onChange={(e) => setProcessor(e.target.value)}>
+                            <option value="Intel Xeon">Intel Xeon</option>
+                            <option value="Intel i9">Intel i9</option>
+                            <option value="Intel i7">Intel i7</option>
+                            <option value="AMD Ryzen 9">AMD Ryzen 9</option>
+                            <option value="AMD Ryzen 7">AMD Ryzen 7</option>
+                        </select><br></br>
 
-            <label for="generation">Processor Generation:</label>
-            <select value={generation} onChange={(e) => setGeneration(e.target.value)}>
-                <option value="13th Gen Intel">13th Gen Intel</option>
-                <option value="12th Gen Intel">12th Gen Intel</option>
-                <option value="11th Gen Intel">11th Gen Intel</option>
-                <option value="AMD Ryzen 7000 Series">AMD Ryzen 7000 Series</option>
-                <option value="AMD Ryzen 6000 Series">AMD Ryzen 6000 Series</option>
-            </select><br></br>
+                        <label for="generation">Processor Generation:</label>
+                        <select value={generation} onChange={(e) => setGeneration(e.target.value)}>
+                            <option value="13th Gen Intel">13th Gen Intel</option>
+                            <option value="12th Gen Intel">12th Gen Intel</option>
+                            <option value="11th Gen Intel">11th Gen Intel</option>
+                            <option value="AMD Ryzen 7000 Series">AMD Ryzen 7000 Series</option>
+                            <option value="AMD Ryzen 6000 Series">AMD Ryzen 6000 Series</option>
+                        </select><br></br>
 
-            <label for="graphics">Graphics:</label>
-            <select value={graphics} onChange={(e) => setGraphics(e.target.value)}>
-                <option value="NVIDIA GeForce RTX 4090">NVIDIA GeForce RTX 4090</option>
-                <option value="NVIDIA GeForce RTX 4080">NVIDIA GeForce RTX 4080</option>
-                <option value="AMD Radeon Pro W6300">AMD Radeon Pro W6300</option>
-                <option value="AMD Radeon Pro W6400">AMD Radeon Pro W6400</option>
-                <option value="Intel Integrated Graphics">Intel Integrated Graphics</option>
-                <option value="Intel UHD Graphics 730">Intel UHD Graphics 730</option>
-                <option value="Intel UHD Graphics 770">Intel UHD Graphics 770</option>
-            </select><br></br>
+                        <label for="graphics">Graphics:</label>
+                        <select value={graphics} onChange={(e) => setGraphics(e.target.value)}>
+                            <option value="NVIDIA GeForce RTX 4090">NVIDIA GeForce RTX 4090</option>
+                            <option value="NVIDIA GeForce RTX 4080">NVIDIA GeForce RTX 4080</option>
+                            <option value="AMD Radeon Pro W6300">AMD Radeon Pro W6300</option>
+                            <option value="AMD Radeon Pro W6400">AMD Radeon Pro W6400</option>
+                            <option value="Intel Integrated Graphics">Intel Integrated Graphics</option>
+                            <option value="Intel UHD Graphics 730">Intel UHD Graphics 730</option>
+                            <option value="Intel UHD Graphics 770">Intel UHD Graphics 770</option>
+                        </select><br></br>
 
-            <button onClick={() => AddComputerToDB(price, memory, storage, processor, generation, graphics, storeName)}>Add Computer</button>
-            </td></tr></tbody></table>
+                        <button onClick={() => AddComputerToDB(price, memory, storage, processor, generation, graphics, storeName)}>Add Computer</button>
+                    </td></tr></tbody></table>
         </div>
     );
 }
