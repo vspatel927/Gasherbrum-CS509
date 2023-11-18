0c98db42e7f76812880d35974312a38fc78f571e
Vansh Patel <vspatel@wpi.edu>
2023-11-17 23:47:19 -0500

    Combining balance + inventory into one table

--- gasherbrumconsignmentsite/src/components/sitemanager/sitemanager.js
+++ gasherbrumconsignmentsite/src/components/sitemanager/sitemanager.js
@@ -7,7 +7,6 @@ function SiteManager() {
   const [listOfStores, setlistOfStores] = useState([]);
   const navigate = useNavigate();
 
-
   useEffect(() => {
     axios
       .get('https://f96at78893.execute-api.us-east-2.amazonaws.com/getStores/getStores')
@@ -31,33 +30,7 @@ function SiteManager() {
         <button name="logout" onClick={() => navigate('/')}>Logout</button>
       </div>
       <br />
-      <BalanceList />
-      <table style={{ width: "75%", margin: "0 auto", marginTop: "0" }}>
-        <thead>
-          <tr>
-            <th colspan="2" class="site-th"><h2>$$ Inventory for Each Store</h2></th>
-          </tr>
-          <tr>
-            <th class="site-th">Store Name</th>
-            <th class="site-th">Inventory Amount ($$)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listOfStores.map((store) => (
-            <tr key={store.name}>
-              <td class="site-td">{store.name}</td>
-              <td class="site-td">${store.inventory}</td>
-              <button name="Delete" onClick={() => deleteStore(store.name)}>Delete</button>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Total Inventory</td>
-            <GetTotalInventory />
-          </tr>
-        </tfoot>
-      </table>
+      <StoreTable />
       <footer class="footer">
         &copy; 2023 Gasherbrum Project Groups
       </footer>
@@ -81,6 +54,41 @@ function SiteManager() {
       });
   }
 
+  function StoreTable() {
+    return (
+      <div>
+        <table style={{ width: "75%", margin: "0 auto", marginTop: "0", borderCollapse: 'collapse', border: '1px solid #ccc' }}>
+          <thead>
+            <tr>
+              <th colspan="2" class="site-th"><h2>Store Information on Site</h2></th>
+            </tr>
+            <tr>
+              <th class="site-th">Store Name</th>
+              <th class="site-th">$$ Balance</th>
+              <th class="site-th">Inventory Amount ($$)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listOfStores.map((store) => (
+              <tr key={store.name}>
+                <td class="site-td">{store.name}</td>
+                <td class="site-td">${store.storeBal}</td>
+                <td class="site-td">${store.inventory}</td>
+                <button name="Delete" onClick={() => deleteStore(store.name)}>Delete</button>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Total Inventory</td>
+              <GetTotalInventory />
+            </tr>
+          </tfoot>
+        </table>
+        <br /><br /> <br />
+      </div>
+    );
+  }
 }
 
 function GetTotalInventory() {
@@ -98,39 +106,5 @@ function GetTotalInventory() {
 
   return <td id="totalBalance">${sum}</td>
 }
-
-function BalanceList() {
-  return (
-    <div>
-      <table style={{ width: "75%", margin: "0 auto", marginTop: "0", borderCollapse: 'collapse', border: '1px sold #ccc' }}>
-        <thead>
-          <th colspan="2" class="site-th" background color="gray"><h2>Balance List</h2></th>
-          <tr>
-            <th class="site-th">Store Name</th>
-            <th class="site-th">Balance</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="site-td">Store 1</td>
-            <td class="site-td">$5</td>
-          </tr>
-          <tr>
-            <td class="site-td">Store 2</td>
-            <td class="site-td">$15</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td><strong>Total Balance</strong></td>
-            <td><strong>$20</strong></td>
-          </tr>
-        </tfoot>
-      </table>
-      <br /><br /> <br />
-    </div>
-  );
-}
-
 
 export default SiteManager
