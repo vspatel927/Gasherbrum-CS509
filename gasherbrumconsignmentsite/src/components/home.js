d228098ab2e1957986c3afbbcc34a135fa4c06f7
Vansh Patel <vspatel@wpi.edu>
2023-11-21 17:59:16 -0500

    Adding computer filter display (not functional yet)

--- gasherbrumconsignmentsite/src/components/home.js
+++ gasherbrumconsignmentsite/src/components/home.js
@@ -25,18 +25,20 @@ function Home() {
   return (
     <div style={{ backgroundColor: 'rgb(60, 194, 185)' }} >
       <Header />
-      <PartFilter />
-      <br />
-      <br />
-
+      {showInventory && <GenerateInventory />}
       <StoreList />
       <br />
       <br />
-
       <StoreInventory />
-
-      {showInventory && <GenerateInventory />}
-      <Footer />
+      <PriceFilter />
+      <MemoryFilter />
+      <StorageFilter />
+      <ProcessorFilter />
+      <ProcessorGenerationFilter />
+      <GraphicsFilter />
+      <br />
+      <br />
+      {/* <Footer /> */}
     </div>
   );
 
@@ -55,21 +57,159 @@ function Home() {
     )
   }
 
-  function PartFilter() {
+  function PriceFilter() {
     return (
-      <div id="filter" style={{ position: 'absolute', left: '10px', backgroundColor: 'gray', color: 'white' }}>
-        <h2>Filter:</h2>
-        <label htmlFor="cpuCheckbox">
-          <input type="checkbox" id="cpuCheckbox" /> CPU
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
+        <h2>Price:</h2>
+        <label htmlFor="2001">
+          <input type="checkbox" id="cpuCheckbox" /> $2,001 or More
         </label>
-        <label htmlFor="ramCheckbox">
-          <input type="checkbox" id="ramCheckbox" /> RAM
+        <br />
+        <label htmlFor="1501">
+          <input type="checkbox" id="ramCheckbox" /> $1,501 - $2,000
         </label>
-        <label htmlFor="modelCheckbox">
-          <input type="checkbox" id="modelCheckbox" /> Model
+        <br />
+        <label htmlFor="1001">
+          <input type="checkbox" id="modelCheckbox" /> $1,001 - $1,500
         </label>
+        <br />
+        <label htmlFor="501">
+          <input type="checkbox" id="modelCheckbox" /> $501 - $1,000
+        </label>
+        <br />
+        <label htmlFor="500">
+          <input type="checkbox" id="modelCheckbox" /> $500 or less
+        </label>
+        <br />
         <div style={{ textAlign: 'left' }}>
-          <button id="searchButton">Search</button>
+          <button id="searchButton">Filter</button>
+        </div>
+      </div>
+    )
+  }
+
+  function MemoryFilter(){
+    return (
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
+        <h2>Memory (RAM):</h2>
+        <label htmlFor="32">
+          <input type="checkbox" id="cpuCheckbox" /> 32 GB or More
+        </label>
+        <br />
+        <label htmlFor="16">
+          <input type="checkbox" id="ramCheckbox" /> 16 GB
+        </label>
+        <br />
+        <label htmlFor="8">
+          <input type="checkbox" id="modelCheckbox" /> 8 GB
+        </label>
+        <br />
+        <label htmlFor="4">
+          <input type="checkbox" id="modelCheckbox" /> 4 GB or Less
+        </label>
+        <br />
+        <div style={{ textAlign: 'left' }}>
+          <button id="searchButton">Filter</button>
+        </div>
+      </div>
+    )
+  }
+
+  function StorageFilter(){
+    return (
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
+        <h2>Storage Size:</h2>
+        <label htmlFor="2TB">
+          <input type="checkbox" id="cpuCheckbox" /> 2 TB or More
+        </label>
+        <br />
+        <label htmlFor="1TB">
+          <input type="checkbox" id="ramCheckbox" /> 1 TB
+        </label>
+        <br />
+        <label htmlFor="512">
+          <input type="checkbox" id="modelCheckbox" /> 512 GB
+        </label>
+        <br />
+        <label htmlFor="256">
+          <input type="checkbox" id="modelCheckbox" /> 256 GB or Less
+        </label>
+        <br />
+        <div style={{ textAlign: 'left' }}>
+          <button id="searchButton">Filter</button>
+        </div>
+      </div>
+    )
+  }
+
+  function ProcessorFilter(){
+    return (
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
+        <h2>Processor:</h2>
+        <label htmlFor="Intel">
+          <input type="checkbox" id="cpuCheckbox" /> All Intel Processors
+        </label>
+        <br />
+        <label htmlFor="AMD">
+          <input type="checkbox" id="ramCheckbox" /> All AMD Processors
+        </label>
+        <br />
+        <div style={{ textAlign: 'left' }}>
+          <button id="searchButton">Filter</button>
+        </div>
+      </div>
+    )
+  }
+
+  function ProcessorGenerationFilter(){
+    return (
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
+      <h2>Processor Generation:</h2>
+      <label htmlFor="13th Gen Intel">
+        <input type="checkbox" id="cpuCheckbox" /> 13th Gen Intel
+      </label>
+      <br />
+      <label htmlFor="12th Gen Intel">
+        <input type="checkbox" id="ramCheckbox" /> 12th Gen Intel
+      </label>
+      <br />
+      <label htmlFor="11th Gen Intel">
+        <input type="checkbox" id="modelCheckbox" /> 11th Gen Intel
+      </label>
+      <br />
+      <label htmlFor="AMD Ryzen 7000 Series">
+        <input type="checkbox" id="modelCheckbox" /> AMD Ryzen 7000 Series
+      </label>
+      <br />
+      <label htmlFor="AMD Ryzen 6000 Series">
+        <input type="checkbox" id="modelCheckbox" /> AMD Ryzen 6000 Series
+      </label>
+      <br />
+      <div style={{ textAlign: 'left' }}>
+        <button id="searchButton">Filter</button>
+      </div>
+    </div>
+    )
+  }
+
+  function GraphicsFilter(){
+    return (
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
+        <h2>Graphics:</h2>
+        <label htmlFor="Nvidia">
+          <input type="checkbox" id="cpuCheckbox" /> All Nvidia Graphics
+        </label>
+        <br />
+        <label htmlFor="AMD">
+          <input type="checkbox" id="ramCheckbox" /> All AMD Graphics
+        </label>
+        <br />
+        <label htmlFor="Intel">
+          <input type="checkbox" id="modelCheckbox" /> All Intel Graphics
+        </label>
+        <br />
+        <div style={{ textAlign: 'left' }}>
+          <button id="searchButton">Filter</button>
         </div>
       </div>
     )
@@ -77,7 +217,7 @@ function Home() {
 
   function StoreList() {
     return (
-      <div id="filter" style={{ position: 'absolute', left: '10px', backgroundColor: 'gray', marginTop: '120px', color: 'white' }}>
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
         <h2>Shops:</h2>
         {listOfStores.map((store) => (
           <label htmlFor="modelCheckbox">
@@ -94,7 +234,7 @@ function Home() {
   function StoreInventory() {
 
     return (
-      <div id="filter" style={{ position: 'absolute', left: '10px', backgroundColor: 'gray', marginTop: '250px', color: 'white' }}>
+      <div id="filter" style={{width: '150px', left: '10px', backgroundColor: 'gray', color: 'white' }}>
         <h2>Inventory:</h2>
         <label htmlFor="allStores">Choose Store:</label>
         <select value={selectedStore} onChange={(e) => setSelectedStore(e.target.value)} style={{ color: 'black' }}>
@@ -102,6 +242,7 @@ function Home() {
             <option value={store.name} key={store.name}>{store.name}</option>
           ))}
         </select>
+        <br />
         <label htmlFor="shopList">All Stores:</label>
 
         <div style={{ textAlign: 'left' }}>
@@ -160,9 +301,11 @@ function Home() {
 
   function Footer() {
     return (
+      <div style={{position: 'absolute', bottom: '0', width: '100%'}}>
       <footer style={{ textAlign: 'center', backgroundColor: 'gray', color: 'white', fontStyle: 'italic', marginTop: '250px' }}>
         &copy; 2023 Gasherbrum Project Groups
       </footer>
+      </div>
     )
   }
 }
